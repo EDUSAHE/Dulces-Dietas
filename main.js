2f86d6ead47a1bc422a80861de238853ded4d412
EDUSAHE <JOSESAHE1@OUTLOOK.COM>
2023-05-26 03:12:28 -0600

    Primer Commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const { app, ipcMain, BrowserWindow } = require("electron");
+// const Store = require("electron-store");
+
+let appWin;
+// const store = new Store();
+
+//If the record does not exist, it is created with a default value of 0.
+// if (!store.get("clicks")) {
+//     store.set("clicks", 0);
+// }
+
+//This function creates the window and its properties.
+createWindow = () => {
+    appWin = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "Angular and Electron",
+        resizable: false,
+        webPreferences: {
+            preload: `${app.getAppPath()}/preload.js`
+        }
+    });
+    
+    appWin.maximize();
+    appWin.loadURL(`file://${__dirname}/dist/index.html`);
+
+    appWin.setMenu(null);
+
+    //appWin.webContents.openDevTools();
+
+    appWin.on("closed", () => {
+        appWin = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => app.quit());
+
+// /* ipcMain is listening the "message" channel, and when the message arrives, 
+//   it replies with "pong" */
+// ipcMain.on("message", (event, message) => { 
+//     if (message === "ping") {
+//         event.reply("reply", "pong"); 
+//     }
+// });
